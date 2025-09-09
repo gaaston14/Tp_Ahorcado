49bc020c29534b55fe1d03ea97d22eddf061e242
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2025-09-08 21:37:40 -0300

    test_arriesgo_palabra_y__no_acierto

--- ahorcado_test.py
+++ ahorcado_test.py
@@ -4,3 +4,7 @@ from ahorcado import arriesgoPalabra
 def test_arriesgo_palabra_y_acierto():
     palabra = arriesgoPalabra("python")
     assert palabra == True
+    
+def test_arriesgo_palabra_y__no_acierto():
+    palabra = arriesgoPalabra("javascript")
+    assert palabra == False
