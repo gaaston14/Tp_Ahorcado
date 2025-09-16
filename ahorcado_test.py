2cdaaadbc1aef39a561d86d07179add2e90d2e91
Camilo <165169940+caamilopereyra01@users.noreply.github.com>
2025-09-15 21:08:27 -0300

    Test Arriesgo Letra Repetida

--- ahorcado_test.py
+++ ahorcado_test.py
@@ -1,6 +1,9 @@
 import pytest
 from ahorcado import arriesgoPalabra,arriesgoLetra,descuentaVida
 
+
+## Test de Funcionalidad Arriesgar Palabra
+
 def test_arriesgo_palabra_y_acierto():
     palabra = arriesgoPalabra("python")
     assert palabra == True
@@ -8,6 +11,9 @@ def test_arriesgo_palabra_y_acierto():
 def test_arriesgo_palabra_y__no_acierto():
     palabra = arriesgoPalabra("javascript")
     assert palabra == False
+
+
+## Test de Funcionalidad Arriesgar Letra
 
 def test_arriesgo_letra_y_acierto():
     letra = arriesgoLetra("y")
@@ -17,9 +23,24 @@ def test_arriesgo_letra_y_no_acierto():
     letra = arriesgoLetra("z")
     assert letra == False
 
+def test_arriesgo_letra_repetida():
+    global letras_acertadas, letras_intentadas
+    letras_acertadas.clear()
+    letras_intentadas.clear()
 
-## Test de Vidas
+    # Primer intento con "p"
+    resultado1 = arriesgoLetra("p")
+    assert resultado1 == True
+
+    # Segundo intento con la misma "p"
+    resultado2 = arriesgoLetra("p")
+    assert resultado2 == "repetida"   # no debería descontar ni modificar nada
+
+
+## Test de Funcionalidad de Vidas
 
 def test_no_acierto_descuenta_vida():
     vida = descuentaVida(arriesgoLetra("z"))
     assert vida == -1
+
+
